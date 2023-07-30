234fb88c54d169e820368036e33f6c64165923c5
AmritpalChera <cheraamritpal@gmail.com>
2023-07-30 11:57:11 -0400

    add copy

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,14 @@ import { ToastContainer } from 'react-toastify'
 
 export const metadata: Metadata = {
   title: 'Mindplug',
-  description: 'An AI brain for any file, any data.',
+  description: 'A virtual brain for smart applications',
+  keywords: ['ai brain', 'embedding generator', 'embedding manager', 'no-code embeddings', 'semantic search', 'ai mind', 'mindplug'],
+  twitter: {
+    images: 'https://image.lexica.art/full_jpg/10080586-52fd-438f-b988-8019fc7582fb'
+  },
+  openGraph: {
+    images: 'https://image.lexica.art/full_jpg/10080586-52fd-438f-b988-8019fc7582fb'
+  }
 }
 
 export default function RootLayout({
