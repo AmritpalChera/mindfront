9a77c4d5fbe01e49a3bebebfd6ec5c398a943882
AmritpalChera <cheraamritpal@gmail.com>
2023-07-30 22:45:27 -0400

    better meta pics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,13 @@ import { ToastContainer } from 'react-toastify'
 
 export const metadata: Metadata = {
   title: 'Mindplug',
-  description: 'A virtual brain for smart applications',
+  description: 'A virtual brain for semantic search on text, pdfs, webpages and audio.',
   keywords: ['ai brain', 'embedding generator', 'embedding manager', 'no-code embeddings', 'semantic search', 'ai mind', 'mindplug'],
   twitter: {
-    images: 'https://image.lexica.art/full_jpg/10080586-52fd-438f-b988-8019fc7582fb'
+    images: 'https://image.lexica.art/full_jpg/1860ab74-3eab-48c5-9a39-88e0fd7a0de0'
   },
   openGraph: {
-    images: 'https://image.lexica.art/full_jpg/10080586-52fd-438f-b988-8019fc7582fb'
+    images: 'https://image.lexica.art/full_jpg/1860ab74-3eab-48c5-9a39-88e0fd7a0de0'
   }
 }
 
